e6b7d83ad94ca6315c581fe102d1a6f8e6763588
jacob-s-codes <jacobbshaul@gmail.com>
2025-09-27 11:11:29 -0700

    added sidebar

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-full w-1/4 text-xl px-4 bg-gray-400'>
+        <p>Dashboard</p>
+        <p>Transactions</p>
+        <p>Goals</p>
+    </div>
+  )
+}
+
+export default Sidebar
